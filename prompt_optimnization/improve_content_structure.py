fd1261d82d3e00168c2e44592658b95aea9fc637
Ryan Zheng <nogrethumphrey@gmail.com>
2023-07-05 15:53:24 +0200

    added one more prompt to improve writting

--- prompt_optimnization/improve_content_structure.py
+++ prompt_optimnization/improve_content_structure.py
@@ -1,60 +1,10 @@
-As a language expert with advanced writing skills, you have been given a prompt in the '>Prompt<' section which is enclosed by `$start$` and `$end$` token. Your task is to thoroughly understand the prompt and evaluate it based on the ordering of words, sentences, and paragraphs. Your evaluation should strive to refine the prompt, ensuring high coherence, syntactic and semantic logical order.
+As a language expert with advanced writing skills and strong logical thinking, you have been given a requirement in the '[Requirement]' section which is enclosed by `$start$` and `$end$` token. Your task is to thoroughly understand the requirement and refine the requirement ensuring high quality of writting.
 
-Evaluate the prompt from the following perspectives:
+You can share from what aspects to improve a given requirement first. The final requirement will be given to a software developer. 
 
-1. Word ordering
-2. Sentence ordering
-3. Paragraph ordering
-Provide your opinions and recommendations for improving the prompt's clarity, effectiveness, sentence structure, content structure, and logical order.
+Think and reason step by step very carefully with high details to address the task. 
 
->Prompt<
+[Requirement]
 $start$
-As a senior software engineer, your task is to enhance the documentation of the given code to improve its readability and maintainability. 
 
-The code is provided within the `>Source Code<`.
-
-The `>Source Code<` section can contain code from multiple files. The section uses the following structure:
-`>File Path<`: Indicates the location of the file in the workspace.
-`>Code<`: Contains the actual code from the file which is encapsulated within `$start$` and `$end$` tokens for clarity.
-
-Here is one example of the `>Source Code<`. The content of example is enclosed within `>example-start<` and `>example-end<`
->example-start<
-
->Source Code<
-
->File Path<: src/sementic_code/index/vectorizer.py
->Code<:
-$start$
-# vectorizer.py
-
-class Vectorizer:
-    def encode(self, data):
-        """
-        Convert the input data (file path, classes, methods, and their descriptions) into vectors.
-        """
-        pass
-
-    def decode(self, vector):
-        """
-        Decode a vector back into its original data.
-        """
-        pass
-
-$end$
-
->File Path<: src/workflow_types/types/workflow_status.py
->Code<:
-$start$
-from enum import Enum
-
-class WorkflowStatus(Enum):
-    """
-    Enumeration representing the status of a workflow.
-    Possible statuses are 'Success', 'Started', and 'Failure'.
-    """
-    Success = 'Success'
-    Started = 'Started'
-    Failure = 'Failure'
-$end$
->example-end<
 $end$
